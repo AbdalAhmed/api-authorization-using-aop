b85ddf5d7911136429014749f2c3f6c996116b90
abdalahmed <abdal.ahmed@careem.com>
2020-05-12 21:25:44 +0500

    Added implementaion for permission authorization

--- src/main/java/com/practice/aop/demo/controller/UserController.java
+++ src/main/java/com/practice/aop/demo/controller/UserController.java
@@ -1,12 +1,14 @@
 package com.practice.aop.demo.controller;
 
 
+import com.practice.aop.demo.annotation.Authorize;
 import com.practice.aop.demo.domain.User;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -16,7 +18,8 @@ import java.util.stream.IntStream;
 public class UserController {
 
     @GetMapping
-    public List<User> getUsers() {
+    @Authorize
+    public List<User> getUsers(HttpServletRequest request) {
         return IntStream.range(0, 10).mapToObj(i ->
                 new User(i, RandomStringUtils.random(10, true, false)))
                 .collect(Collectors.toList());
---
+++ src/main/java/com/practice/aop/demo/security/PermissionAuthImpl.java
@@ -0,0 +1,13 @@
+package com.practice.aop.demo.security;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PermissionAuthImpl {
+
+    public boolean authorize(String apiKey) {
+
+        return false;
+    }
+}
